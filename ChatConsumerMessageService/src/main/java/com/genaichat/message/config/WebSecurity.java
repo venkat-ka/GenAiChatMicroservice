381c8e2077918fc9c6538bef0b208a3ad4fa8e48
venkat-ka <venkat.k.2516@gmail.com>
2025-05-16 22:43:24 +0530

    [+] websecurity for consumer

---
+++ ChatConsumerMessageService/src/main/java/com/genaichat/message/config/WebSecurity.java
@@ -0,0 +1,46 @@
+package com.genaichat.message.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+	private Environment environment;
+	
+	
+    
+    public WebSecurity(Environment environment) {
+		super();
+		this.environment = environment;
+	}
+
+
+
+	@Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    	
+    	// Configure AuthenticationManagerBuilder
+    	http.csrf((csrf) -> csrf.disable());
+    	http
+        .authorizeHttpRequests((authorize) -> authorize
+            .anyRequest().permitAll()
+        );
+    	
+		http.sessionManagement((session) -> session
+		        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.sameOrigin()));
+        return http.build();
+    }
+}
